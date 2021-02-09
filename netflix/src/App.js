345d9ebf68ccf15c59e35a0b88ae55f00226bfe9
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-02-09 02:15:35 -0500

    newfileadded

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -4,9 +4,15 @@ import Row from './Row';
 
 function App() {
   return (
-		<div className='App'>
-      <Row title="NETFLIX ORIGNALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+		<div className="App">
+			<Row title="NETFLIX ORIGNALS" fetchUrl={requests.fetchNetflixOriginals} />
+			<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horrer Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 		</div>
 	);
 }

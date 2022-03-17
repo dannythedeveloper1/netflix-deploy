d0d814e9c08e3eba13674bb33cc22f2bfa04c3ff
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-03-16 21:18:03 -0400

    hide api key

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "e1be22fc273a3079dd38ee671558f1ef";
+const API_KEY = process.env.REACT_APP_apikey;
 const requests = {
 	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
 	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
